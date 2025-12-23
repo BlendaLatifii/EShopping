c449b9e10728c88580d333f0fe12e1568f0b91f5
BlendaLatifii <blendalatifi4@gmail.com>
2025-12-23 11:55:53 +0100

    setup project architecture

---
+++ EShopping/Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Repositories;
+using Infrastructure.Repositories.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static async void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IUserRepository, UserRepository>();
+        }
+    }
+}
